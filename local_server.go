e7939c86a6d855e0fa0983c0f71b016df70a8ae2
Elesh-Norn <kercyan@live.fr>
2022-03-23 16:32:28 +0100

    Add functions to dump fetched entries to a json and load them
    I added this two functions because I was thinking I could build this website once a day
    and then just build the html page from the json. I'm realising I can just create the html page instead.
    I will still commit this code, because it might become useful if I add persistence to the app.

--- local_server.go
+++ local_server.go
@@ -1,39 +1,17 @@
 package main
 
 import (
-  "fmt"
   "net/http"
   "html/template"
 )
 
-
-func hello (writer http.ResponseWriter, req *http.Request) {
-  fmt.Fprintf(writer, "hello\n")
-}
-
-func headers(writer http.ResponseWriter, req *http.Request) {
-  for name, headers := range req.Header {
-    for _, h := range headers {
-      fmt.Fprintf(writer, "%v: %v\n", name, h)
-    }
-  }
-}
-
 func reader(writer http.ResponseWriter, req *http.Request) {
-  addresses := []string{
-    "https://emberger.xyz/index.xml",
-    //"https://martinfowler.com/feed.atom",
-  }
   t := template.Must(template.ParseFiles("layout.html"))
-  feeds := getFeeds(addresses)
-  entries:= getSortedEntries(feeds)
+  entries:= load_entries()
   t.Execute(writer, entries)
-
 }
 
-func main() {
-  http.HandleFunc("/hello", hello)
-  http.HandleFunc("/headers", headers)
-  http.HandleFunc("/reader", reader)
+func main(){
+  http.HandleFunc("/", reader)
   http.ListenAndServe(":8090", nil)
 }
